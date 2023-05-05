55632fc260376871db733a320d8d88868d69fe10
Rutuja Kavade <118648971+Rutz53@users.noreply.github.com>
2023-05-05 11:50:26 +0530

    Add files via upload

---
+++ CurvedSurfaceArea.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class CurvedSurfaceArea
+{
+public static void main(String args[])
+{
+double s,a;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter side:");
+s=sc.nextDouble();
+a=6*s*s;
+System.out.println("Curved surface area of cube="+ a);
+}
+}
---
+++ VolumeOfSphere.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class VolumeOfSphere
+{
+public static void main(String args[])
+{
+double r,v;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter radius:");
+r=sc.nextDouble();
+
+v=4*3.14*r*r*r/3;
+System.out.println("Volume of Sphere="+v);
+}
+}
