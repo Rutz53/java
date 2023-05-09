ec9ef02074d69fc5e9077fee9616a42f5c47d054
Rutuja Kavade <118648971+Rutz53@users.noreply.github.com>
2023-05-09 19:11:18 +0530

    Add files via upload

---
+++ programs2/PowerInJava.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class PowerInJava
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double b,p;
+System.out.println("Enter value of base:");
+b=sc.nextDouble();
+System.out.println("Enter value of exponent:");
+p=sc.nextDouble();
+System.out.println(Math.pow(b,p));
+}
+}
