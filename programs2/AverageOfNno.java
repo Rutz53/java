d4f88c87593ca61bb0423b29b094c50efa128612
Rutuja Kavade <118648971+Rutz53@users.noreply.github.com>
2023-05-09 18:57:22 +0530

    Add files via upload

---
+++ programs2/AverageOfNno.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class AverageOfNno
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double i,n,sum=0,avg;
+i=1;
+System.out.println("Enter n:");
+n=sc.nextDouble();
+
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}avg=sum/n;
+System.out.println("s="+avg);
+}
+}
