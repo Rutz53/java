a858dfdc63d2fc768771602a23bb0417e77e56ab
Rutuja Kavade <118648971+Rutz53@users.noreply.github.com>
2023-05-11 09:38:11 +0530

    Add files via upload

---
+++ programs2/AdditionNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class AdditionNumbers
+{
+public static void main(String args[])
+{
+double a,b,s;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter two numbers:");
+a=sc.nextDouble();
+b=sc.nextDouble();
+s=a+b;
+System.out.println("Addition of two numbers="+s);
+}
+}
